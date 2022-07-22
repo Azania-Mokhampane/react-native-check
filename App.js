a2f83a0cb94a4341e17009caf5311a2b26eb56ba
Azania Mokhampane <azania@nexl.cloud>
2022-07-22 15:55:58 +0200

    initial commit

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { AntDesign, Ionicons } from '@expo/vector-icons';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import {windowHeight, windowWidth} from "./utils"
 export default function App() {
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.header}>
+        <AntDesign name="left" size={24} color="white" />
+        <Text style={styles.text}>Transfer Money</Text>
+        <Ionicons name="ios-information-circle-outline" size={24} color="white" />
+      </View>
+
+      <View>
+        <Text style={styles.text}>SEND TO:</Text>
+        <Image style={styles.image} source={require("")} />
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +24,23 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'black',
+    height: windowHeight,
+    flex:1
   },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    width: windowWidth,
+    top:40,
+  },
+  text: {
+    color: "white"
+  },
+  image: {
+    width: 66,
+    height: 58,
+    bottom: 200
+  }
 });
