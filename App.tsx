9c2e8778f64b03bee649eb1543fdb4901fdf519e
Azania-Mokhampane <azania@nexl.cloud>
2022-07-24 18:15:14 +0200

    feat: navigations

---
+++ App.tsx
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import HomeScreen from './screens/HomeScreen';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LandingScreen from './screens/LandingScreen';
+export default function App() {
+
+  const Stack = createNativeStackNavigator();
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Landing Screen'>
+      <Stack.Screen name='Home' component={HomeScreen} />
+      <Stack.Screen name='Landing Screen' component={LandingScreen} />
+      </Stack.Navigator>
+      <StatusBar />
+    </NavigationContainer>
+      
+  );
+}
+
+
+
