66e9343689f14fb76546093bb8a3b27b85fd4f2f
Azania Dev <azaniam04@gmail.com>
2022-07-30 10:10:29 +0200

    ref: align buttons

--- App.tsx
+++ App.tsx
@@ -1,25 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import HomeScreen from './screens/HomeScreen';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LandingScreen from './screens/LandingScreen';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import HomeScreen from "./screens/HomeScreen";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LandingScreen from "./screens/LandingScreen";
+import LoginScreen from "./screens/LoginScreen";
+import SignUpScreen from "./screens/SignUpScreen";
 export default function App() {
-
   const Stack = createNativeStackNavigator();
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{
-        headerShown:true
-      }} initialRouteName='Landing Screen'>
-      <Stack.Screen name='Home' component={HomeScreen} />
-      <Stack.Screen name='Landing Screen' component={LandingScreen} />
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: true,
+        }}
+        initialRouteName="Landing Screen"
+      >
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Landing Screen" component={LandingScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Sign Up" component={SignUpScreen} />
       </Stack.Navigator>
       <StatusBar />
     </NavigationContainer>
-      
   );
 }
-
-
-
